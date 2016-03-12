ef702773c2921b8634d0f8994fd32da85ef90bb3
Noah Emmet <noah@noahemmet.com>
2016-03-11 17:55:30 -0800

    Initial Commit

---
+++ WorldKit/WorldKit.h
@@ -0,0 +1,19 @@
+//
+//  WorldKit.h
+//  WorldKit
+//
+//  Created by Noah Emmet on 3/11/16.
+//  Copyright © 2016 Sticks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WorldKit.
+FOUNDATION_EXPORT double WorldKitVersionNumber;
+
+//! Project version string for WorldKit.
+FOUNDATION_EXPORT const unsigned char WorldKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorldKit/PublicHeader.h>
+
+
