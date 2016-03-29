30951f92e7fde250f478b3ade8e58536798fd737
Noah Emmet <noah@noahemmet.com>
2016-03-29 02:10:05 -0700

    spiraling

--- WorldKit/Classes/MatrixPlayground.playground/Contents.swift
+++ WorldKit/Classes/MatrixPlayground.playground/Contents.swift
@@ -16,6 +16,7 @@ let world = World(rows: 9, columns: 9)
 let worldSequence = WorldSequence(initial: world)
 let worldView = WorldView(worldSequence: worldSequence)
 XCPlaygroundPage.currentPage.liveView = worldView
+
 // elementsAt
 
 // elements at
@@ -25,15 +26,23 @@ XCPlaygroundPage.currentPage.liveView = worldView
 
 // extended selection
 
-let rings = world.cells.extendedSelection(around: MatrixPoint(row: 5, column: 5))
-
+//let rings = world.cells.extendedSelection(around: MatrixPoint(row: 4, column: 4), range: 1..<4)
+//
+//var hue:  CGFloat = 0.5
+//var saturation: CGFloat = 0.8
+//let colorIncrement: CGFloat = 0.05
+//for (r, ring) in rings.enumerate() {
+//	for (i, cell) in ring.enumerate() {
+//		cell.color = NSColor(hue: hue, saturation: saturation, brightness: 0.8, alpha: 1.0)
+//	}
+//	hue += colorIncrement
+//	saturation -= colorIncrement
+//}
 var hue:  CGFloat = 0.5
 var saturation: CGFloat = 0.8
-let colorIncrement: CGFloat = 0.05
-for (r, ring) in rings.enumerate() {
-	for (i, cell) in ring.enumerate() {
-		cell.color = NSColor(hue: hue, saturation: saturation, brightness: 0.8, alpha: 1.0)
-	}
+let colorIncrement: CGFloat = 0.01
+for cell in world.cells.spiral(from: MatrixPoint(row: 4, column: 4)) {
+	cell.color = NSColor(hue: hue, saturation: saturation, brightness: 0.8, alpha: 1.0)
 	hue += colorIncrement
 	saturation -= colorIncrement
 }
@@ -65,7 +74,6 @@ extension Matrix {
 	func extendedSelection(around centerPoint: MatrixPoint, range: Range<Int> = 1..<2) -> [[Element]] {
 		var rings: [[Element]] = []
 		for ringIndex in range {
-			var ring: [Element] = []
 			
 			// Corners
 			let topLeft = MatrixPoint(row: centerPoint.row - ringIndex, column: centerPoint.column + ringIndex)
@@ -74,39 +82,43 @@ extension Matrix {
 			let bottomLeft = MatrixPoint(row: centerPoint.row - ringIndex, column: centerPoint.column - ringIndex)
 			
 			// Edges
-			let topRow: [Element] = elementsAt(column: topLeft.column, range: topLeft.row+1..<bottomRight.row+1)
-			let rightColumn: [Element] = elementsAt(row: bottomRight.row, range: bottomRight.column..<topRight.column)
-			let bottomRow: [Element] = elementsAt(column: bottomLeft.column, range: bottomLeft.row..<bottomRight.row)
-			let leftColumn: [Element] = elementsAt(row: bottomLeft.row, range: bottomLeft.column+1..<topLeft.column+1)
+			let offset = 1
+			let topRow: [Element] = elementsAt(column: topLeft.column, range: topLeft.row+offset..<bottomRight.row+offset)
+			let rightColumn: [Element] = elementsAt(row: bottomRight.row, range: bottomRight.column..<topRight.column).reverse()
+			let bottomRow: [Element] = elementsAt(column: bottomLeft.column, range: bottomLeft.row..<bottomRight.row).reverse()
+			let leftColumn: [Element] = elementsAt(row: bottomLeft.row, range: bottomLeft.column+offset..<topLeft.column+offset)
 			
-			rings.append(topRow)
-			rings.append(rightColumn)
-			rings.append(bottomRow)
-			rings.append(leftColumn)
+			let ring: [Element] = topRow + rightColumn + bottomRow + leftColumn
+			rings.append(ring)
 		}
 		return rings
 	}
 	
-	//	public func circle(from centerPoint: MatrixPoint, range: Range<Int>) -> Set<
-	
-//	func spiral(from centerPoint: MatrixPoint, start cardinality: PrincipalCardinalDirection = .NorthWest, clockwise: Bool = true) -> AnyGenerator<Element> {
-//		var isFirstElement = true
-//		var nextPoint = centerPoint
-//		var ring = 1
-//		let directions = cardinality.cases
-//		var nextDirection = cardinality
-//		//		var traversalDirection = 
-//		return AnyGenerator<Element> {
-//			/// Distance to travel per edge, sans overlap. 
-//			let ringLength = ring * 2
-//			let range = 
-//				print(nextDirection)
-//			nextDirection = nextDirection.generate().next()!
-//			if let point = self[point: nextPoint, cardinality: nextDirection, distance: 1] {
-//				print(point)
-//				return self[point.row, point.column] // this seems backwards
-//			}
-//			return nil
-//		}
-//	}
+	public func spiral(from centerPoint: MatrixPoint, start cardinality: PrincipalCardinalDirection = .NorthWest, clockwise: Bool = true) -> AnyGenerator<Element> {
+
+		var ringIndex = 1
+		var elementIndex = 0
+		var nextRing = extendedSelection(around: centerPoint).first!
+		return AnyGenerator<Element> { 
+			if elementIndex < nextRing.count {
+				// Traverse each ring
+				let element = nextRing[elementIndex]
+				elementIndex += 1
+				return element
+			} else {
+				// end of ring
+				ringIndex += 1
+				elementIndex = 0
+				let nextRange = ringIndex..<ringIndex+1
+				if let possibleNextRing = self.extendedSelection(around: centerPoint, range: nextRange).first where !possibleNextRing.isEmpty {
+					nextRing = possibleNextRing
+					let element = nextRing[0]
+					return element
+				} else {
+					// All elements are out of bounds.
+					return nil
+				}
+			}
+		}
+	}
 }
